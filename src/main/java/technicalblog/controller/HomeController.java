1c55dc73376eb790115e58cba0cd1a1f5964e12a
Pof navvenraj <navvenr96@gmail.com>
2020-09-29 19:26:14 +0530

    techblog

---
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -0,0 +1,36 @@
+package technicalblog.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import technicalblog.model.Post;
+import technicalblog.service.PostService;
+
+
+
+@Controller
+public class HomeController {
+	
+	public HomeController() {
+		System.out.println("*** HomeController ***");
+    }
+
+	@Autowired
+	private PostService postservice;
+
+	@RequestMapping("/")
+	public String getAllPosts(Model model) {
+		// TODO Auto-generated method stub
+		ArrayList<Post> posts = postservice.getAllPosts();
+		model.addAttribute("posts", posts);
+		return "index";
+	}
+
+}
+
+
+
---
+++ src/main/java/technicalblog/controller/PostController.java
@@ -0,0 +1,26 @@
+package technicalblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    private PostService postservice;
+
+    @RequestMapping("posts")
+    public String getUserPosts(Model model){
+        ArrayList<Post> posts = postservice.getOnePost();
+        model.addAttribute("posts", posts);
+        return "posts";
+    }
+
+
+}
